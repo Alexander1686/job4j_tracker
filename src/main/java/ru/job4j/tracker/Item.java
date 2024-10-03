b7a55de878f0a8802dc9ea7cc46b643058bb91c1
1686 <1686@inbox.ru>
2024-10-04 04:13:47 +0500

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,41 +1,35 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     public LocalDateTime created;
-    //private LocalDateTime created;
 
     public Item() {
-        //this.created = LocalDateTime.now();
+
     }
 
     public Item(String name) {
         this.name = name;
-       // this.created = LocalDateTime.now();
+
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-       // this.created = LocalDateTime.now();
+
     }
 
     public Item(int id, String name, LocalDateTime created) {
         this.id = id;
         this.name = name;
-     //   this.created = LocalDateTime.now();
+        this.created = LocalDateTime.now();
+
     }
 
     public LocalDateTime getCreated() {
-        this.created = LocalDateTime.now();
-        return created;
-    }
-
-    public LocalDateTime getCreated2() {
         this.created = LocalDateTime.now();
         return created;
     }
@@ -54,14 +48,6 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public static void main(String[] args) {
-        Item item = new Item("Sample Item");
-        System.out.println("time and data now " + item.getCreated());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String created = item.created.format(formatter);
-        System.out.println("time and data now in right format " + created);
     }
 }
 
